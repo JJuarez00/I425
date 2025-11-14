3468182f5e072f1e9a4a07a8a4a4f920470652dc
Joseph <josephjuarez455@gmail.com>
2025-11-14 00:38:59 -0500

    Add files via upload

---
+++ practice-7-mycollege-spa/src/config/routes.jsx
@@ -0,0 +1,39 @@
+/**
+ * Author: Joseph Juarez
+ * Date: 11/05/2025
+ * File: routes.jsx
+ * Description: Defines app routes.
+ */
+
+import {BrowserRouter,Routes,Route}from "react-router-dom";
+import Layout from "../components/Layout";
+import Home from "../pages/home";
+import NoMatch from "../pages/nomatch";
+import Professors from "../pages/professor/professors.jsx";
+import Professor from "../pages/professor/professor.jsx";
+import Classes from "../pages/class/classes.jsx";
+
+const AppRoutes = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Home/>}/>
+
+                    <Route path="professors" element={<Professors/>}>
+                        <Route index element={<p>Select a professor to view details.</p>}/>
+
+                        <Route path=":professorId" element={<Professor/>}>
+                            <Route path="classes" element={<Classes />}/>
+                        </Route>
+
+                    </Route>
+
+                    <Route path="*" element={<NoMatch/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default AppRoutes;
---
+++ practice-7-mycollege-spa/src/pages/class/classes.jsx
@@ -0,0 +1,58 @@
+/**
+ * Author: Joseph Juarez
+ * Date: 11/11/2025
+ * File: classes.jsx
+ * Description: Display classes taught by a professor.
+ */
+
+import {settings} from "../../config/config";
+import useXmlHttp from '../../services/useXmlHttp';
+import {useParams} from "react-router-dom";
+import '../../assets/css/class.css';
+
+const Classes = () => {
+
+    const {professorId} = useParams();
+    const url = settings.baseApiUrl + "/professors/" + professorId + "/classes";
+    const {
+        error,
+        isLoading,
+        data: classes
+    } = useXmlHttp(url);
+
+    return (
+        <>
+            {error && <div>{error}</div>}
+
+            {isLoading &&
+                <div className="image-loading">
+                    Please wait while data is being loaded
+                    <img src="/src/assets/img/loading.gif" alt="Loading ......"/>
+                </div>}
+
+            {classes && (classes.length === 0
+                    ? <p>Classes were not found.</p>
+                    : <div className="class-row class-row-header">
+                        <div>Course</div>
+                        <div>Section</div>
+                        <div>Semester</div>
+                        <div>Year</div>
+                    </div>
+            )}
+
+            {classes && (
+                classes.map((cls, index) => ( //cannot use "class" as a variable name. "class" is a reserved word/
+                    <div key={index} className="class-row">
+                        <div>{cls.course}</div>
+                        <div>{cls.section}</div>
+                        <div>{cls.semester}</div>
+                        <div>{cls.year}</div>
+                    </div>
+                ))
+            )}
+
+        </>
+    );
+};
+
+export default Classes;
